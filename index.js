427252c4147433d28acf3a983ceb2fa09dbf2178
Mr-Robot-101 <git.sujon@gmail.com>
2022-10-20 11:09:32 +0600

    Api ready

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const exprees= require('express')
 const app= exprees()
 const port= process.env.PORT || 5000
-const catagories= require('./data/catagories.json')
 const cors= require('cors')
-
+const catagories= require('./data/catagories.json')
+const news= require('./data/news.json')
 app.use(cors())
 
 
@@ -14,6 +14,27 @@ app.get('/', (req, res) => {
 app.get('/news-catagories/', (req, res) => {
     res.send(catagories)
 })
+
+app.get('/news/:id', (req,res )=>{
+    const id=req.params.id
+    const selectedNews= news.find(n =>n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/catagories/:id', (req, res)=> {
+    const categoryId= req.params.id
+
+    if (categoryId === '08') {
+        res.send(news)
+    }
+    else{
+        const categoryNews= news.filter(category=> category.category_id === categoryId)
+        res.send(categoryNews)
+    }
+   
+})
+
+
 
 
 app.listen(port, ()=> {
